c214135d9de23ec54d71f7bec6a0d8b027f15292
huesoluis <huesoluis@gmail.com>
2020-03-30 14:13:33 +0200

    version12

--- config/config_global.php
+++ config/config_global.php
@@ -2,7 +2,10 @@
 define("CONTROLADOR_DEFECTO", "centros");
 define("ACCION_DEFECTO", "index");
 
-define("DIR_BASE",$_SERVER['CONTEXT_DOCUMENT_ROOT']."/");
+if(isset($_SERVER['CONTEXT_DOCUMENT_ROOT']))
+	define("DIR_BASE",$_SERVER['CONTEXT_DOCUMENT_ROOT']."/");
+else
+	define("DIR_BASE","/datos/www/preadmespecial.aragon.es/public_admespecial/");
 define("DIR_CORE",DIR_BASE."/core");
 #define("IPREMOTA","172.27.0.56");
 define("IPREMOTA","90.94.107.29");
@@ -16,19 +19,22 @@ define("DATA_SCRIPTS_DIR",'/scripts/datos_entrada/');
 
 define("URL_BASE",'http://preadmespecial.aragon.es/');
 
+define("VERSION",'PRE');
+
+define("DIA_INICIO_PRORROGA",'2020/03/02');
 define("DIA_INICIO",'2020/03/04');
-define("DIA_INICIO_INSCRIPCION",'2020/03/04');
-define("DIA_FIN_INSCRIPCION",'2020/03/05');
-define("DIA_MAX_SOL_ALUMNO",'2020/03/05');
+define("DIA_INICIO_INSCRIPCION",'2020/03/09');
+define("DIA_MAX_SOL_ALUMNO",'2020/03/23');
+define("DIA_FIN_INSCRIPCION",'2020/03/25');
 
-define("DIA_SORTEO",'2020/03/04');
+define("DIA_SORTEO",'2020/03/26');
 
-define("DIA_BAREMACION",'2020/03/06');
-define("DIA_PUBLICACION_BAREMACION",'2020/03/07');
+define("DIA_BAREMACION",'2020/03/27');
+define("DIA_PUBLICACION_BAREMACION",'2020/03/27');
 
-define("DIA_PROVISIONALES",'2020/03/07');
-define("DIA_DEFINITIVOS",'2020/04/16');
+define("DIA_PROVISIONALES",'2020/03/29');
+define("DIA_DEFINITIVOS",'2020/03/30');
 
-define("DIA_SORTEO_SP",'2020/04/22');
+define("DIA_SORTEO_FASE2",'2020/03/30');
 
 ?>
---
+++ js/fase2.js
@@ -0,0 +1,113 @@
+$(document).ready(function(){
+//LISTADO SOLICITUDES FASEII
+$(".lfase2").click(function () {  
+  var vsorteo_fase2=$('#sorteo_fase2').text();
+  var vpdf='1';
+  var vrol=$('#rol').attr("value");
+  var vsubtipo=$(this).attr("data-subtipo");
+  var vestado_convocatoria=$('#estado_convocatoria').val();
+	$.ajax({
+	  method: "POST",
+	  url: "../scripts/ajax/listados_solicitudes_fase2.php",
+	  data: {asignar:'0',rol:vrol,subtipo:vsubtipo,pdf:vpdf,estado_convocatoria:vestado_convocatoria,sorteo_fase2:vsorteo_fase2},
+	  success: function(data) {
+					$("#l_matricula").html(data);
+					$(".container").hide();
+	      },
+	      error: function() {
+		alert('Error LISTANDO solicitudes: '+vsubtipo);
+	      }
+	});
+});
+
+//ASIGNAR NUMERO FASE2
+$('body').on('click', '#boton_asignar_numero_fase2', function(e){
+var vsorteo_fase2=$('#sorteo_fase2').text();
+var vrol=$('#rol').attr("value");
+var vsubtipo=$(this).attr("data-subtipo");
+var vestado_convocatoria=$('#estado_convocatoria').text();
+var vidcentro=$('#id_centro').text();
+	$.ajax({
+	  method: "POST",
+	  data: {asignar:'1',rol:vrol,subtipo:vsubtipo,estado_convocatoria:vestado_convocatoria,sorteo_fase2:vsorteo_fase2},
+	  url:'../scripts/ajax/listados_solicitudes_fase2.php',
+	      success: function(data) {
+			        $("#tresumen"+vidcentro).remove();
+                                $("#filtroscheck").remove();
+                                $("#nuevasolicitud").remove();
+                                $("#form_sorteo").remove();
+                                $("#sol_table").remove();
+				$("#l_matricula").html(data);
+                                $("#num_sorteo").prop("disabled",false);
+		},error: function (request, status, error) {
+        alert(error);
+    }
+	});
+});
+
+//REALIZAR SORTEO
+
+$('body').on('click', '#boton_realizar_sorteo_fase2', function(e){
+
+var vestado_convocatoria=$('#estado_convocatoria').text();
+var vid=$(this).attr("id");
+var vrol=$('#rol').attr("value");
+var vidcentro=$('#id_centro').text();
+var vsolicitudes=$(this).attr("data-solicitudes");
+var vnum_sorteo=$('#num_sorteo').val();
+var vnum_solicitudes=$('#num_solicitudes').val();
+var isnum = /^\d+$/.test(vnum_sorteo);
+if (!isnum) {
+    alert('No es un numero');
+return;
+}
+if (parseInt(vnum_solicitudes)<parseInt(vnum_sorteo) || parseInt(vnum_sorteo)<=0) {
+    alert('Introduce un numero entre 1 y '+vnum_solicitudes);
+return;
+}
+	$.ajax({
+	  method: "POST",
+	  data: {asignar:'2',rol:vrol,nsorteo:parseInt(vnum_sorteo),estado_convocatoria:vestado_convocatoria},
+	  url:'../scripts/ajax/listados_solicitudes_fase2.php',
+	      success: function(data) {
+				console.log(data);
+				$("#l_matricula").html(data);
+				$("#tresumen").hide();
+		},
+	      error: function() {
+		alert('Problemas listando solicitud!');
+	      }
+	});
+});
+
+var cen_options = 
+	{
+	url: "../datosweb/centros_especial.json",
+	getValue:"nombre_centro",
+		list: 
+		{
+			onSelectItemEvent: function() {
+			var nombre = $("#fcentrosadmin").getSelectedItemData().nombre_centro;
+			var idcentro = $("#fcentrosadmin").getSelectedItemData().id_centro;
+			$("#id_centro").text(idcentro);
+			$("#id_centro").attr("value",idcentro);
+			$("#rol").text('centro');
+			$("#rol").attr("value","centro");
+			},
+			maxNumberOfElements: 10,
+			match: 
+			{
+			enabled: true
+			},
+			onKeyEnterEvent: function() 
+			{
+			var vcentro = $('#buscar_centros').getSelectedItemData().name;
+			},
+			onClickEvent: function() 
+			{
+			var vcentro = $('#buscar_centros').getSelectedItemData().name;
+			}
+		}
+	};
+$(".cdefinitivo").easyAutocomplete(cen_options);
+});
