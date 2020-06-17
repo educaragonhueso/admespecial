34e29f29fdc7703e67fb77b69db7264850ff964b
huesoluis <huesoluis@gmail.com>
2020-06-17 10:03:27 +0200

    tri2

--- guarderias/js/tributantes.js
+++ guarderias/js/tributantes.js
@@ -20,26 +20,6 @@ if(!isNumber(vimporte_renta1))
   return;
 }
 vimporte_renta1=parseFloat(vimporte_renta1);
-/*
-if(!isFloat(vimporte_renta1))
-{
-   alert("INTRODUCE VALOR NUMéRICO");
-  return;
-}
-
-var reg1 = /^\d+$/;
-var reg2 = /^\d+\.+\d+$/;
-if(!reg2.test(vimporte_renta1)){
-  alert("INTRODUCE VALOR NUMéRICO");
-  return;
-}
-*/  
-/*
-if(!reg1.test(vimporte_renta1) & !reg2.test(vimporte_renta1)){
-  alert("INTRODUCE VALOR NUMéRICO");
-  return;
-}
-*/
 vimporte_renta=parseFloat(vimporte_renta1);
 
 var vid=$(this).attr("id");
@@ -49,10 +29,15 @@ if(vimporte_renta<viprim)
    $('#puntos_renta'+vid).text('1.5');
    $('#puntos_renta'+vid).val('1.5');
    }
-else
+else if(vimporte_renta<2*viprim)
    {
    $('#puntos_renta'+vid).text('1');
    $('#puntos_renta'+vid).val('1');
+   }
+else
+   {
+   $('#puntos_renta'+vid).text('0');
+   $('#puntos_renta'+vid).val('0');
    }
 var cuota=0;
 if(vimporte_renta>314 & vimporte_renta<=532) 
@@ -66,48 +51,6 @@ $('#cuota'+vid).text(cuota);
 $('#cuota'+vid).val(cuota);
 return;
 });
-/*
-  vidcentro=$(this).parent('td').parent('tr').parent('tbody').parent('table').attr('id');
-  vidcentro=vidcentro.replace('mat_table','');
-  var ots = $(this);
-  var vid=$(this).attr("id");
-  vid=vid.replace('cambiar','');
-  var vcontinua=$("#estado"+vid).text();
-  var vtipoalumno=$('#tipoalumno'+vid).text();
-  var vestado_pulsado=$(this).text();
-  var vestado_actual=$(this).parent('div').parent('div').attr("id");
-$.ajax({
-  method: "POST",
-  data: { id_alumno:vid,estado_pulsado:vestado_pulsado,estado_actual:vestado_actual,id_centro:vidcentro,continua:vcontinua},
-  url:'../guarderias/scripts/ajax/cambio_estado_solicitud.php',
-      success: function(data) 
-      {
-	if(vcontinua.indexOf('NO')!=-1){ alert("El alumno no continua, no afecta plazas vacantes");return;}
-	cambiar_tipo(ots,vestado_pulsado,vid);
-	var vacantes_ebo =data.split(":")[0];
-	var vacantes_tva =data.split(":")[1];
-       	$('#vacantesmat_ebo_desk'+vidcentro).html(vacantes_ebo);
-   	$('#vacantesmat_tva_desk'+vidcentro).html(vacantes_tva);
-   	var  npo_ebo=$('#vacantesmat_ebo_desk'+vidcentro).prev().text();
-   	var  npo_tva=$('#vacantesmat_tva_desk'+vidcentro).prev().text();
-	if(vestado_pulsado.indexOf('EBO')!=-1)
-	{
-		//$(this).closest('#vacantesmat_ebo_desk').prev().html(+npo_ebo+1);
-		$('#vacantesmat_ebo_desk'+vidcentro).prev().html(+npo_ebo+1);
-		$('#vacantesmat_tva_desk'+vidcentro).prev().html(+npo_tva-1);
-		//$('#vacantesmat_tva_desk').prev().html(+npo_tva-1);
-	}
-	if(vestado_pulsado.indexOf('TVA')!=-1)
-	{
-		$('#vacantesmat_ebo_desk'+vidcentro).prev().html(+npo_ebo-1);
-	   	$('#vacantesmat_tva_desk'+vidcentro).prev().html(+npo_tva+1);
-	}
-      },
-      error: function() {
-        alert('Problemas cambiando de estado!');
-      }
-});
-*/
 //GRABAMOS RENTA TRIBUTANTES
 $('body').on('click', '.setrenta', function(e){
   var vrol=$('#rol').attr("value");
